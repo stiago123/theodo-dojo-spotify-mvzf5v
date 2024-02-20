c3397f67f848ea9c1785857c58f006c7b4c307ce
stiago123 <smarinoj@unal.edu.co>
2024-02-20 21:04:23 +0100

    idadas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { fetchTracks } from './lib/fetchTracks';
 import { useQuery } from '@tanstack/react-query';
 
+
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
@@ -37,7 +38,7 @@ const App = () => {
       <div className="App-images">
       <audio src={trackUrls[trackIndex]} autoPlay controls />
         
-        <p>C'est mon premiere apli musicalee</p>
+        <p>C'est mon premiere apli musicalee </p>
       </div>
       <div className="App-buttons">
       <button onClick={goToNextTrack}>
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,6 +1,9 @@
-const apiToken = 'BQDfAZg8yOKsEU7lL8rNnDIL2fSoyHhUf2KrpAFGumre6Q1_0Oj-w_ExnGcIcRJGcawIWHM2YUzFudHHB8ch7FxM8qGOl1Z6d6dA0Zq0qUgp9Ccga19C0fi2mTTYwqp_II5Z4NzcDqHLjhnTZY3p9I1W4Qv0Kk_e2FyH1Aa0rWkmr3wapwoycLSc9sJBuZ2BcZLqg0zy  ';
+import { SavedTrack } from 'spotify-types'; 
 
-export const fetchTracks = async () => {
+const apiToken = 'BQDfAZg8yOKsEU7lL8rNnDIL2fSoyHhUf2KrpAFGumre6Q1_0Oj-w_ExnGcIcRJGcawIWHM2YUzFudHHB8ch7FxM8qGOl1Z6d6dA0Zq0qUgp9Ccga19C0fi2mTTYwqp_II5Z4NzcDqHLjhnTZY3p9I1W4Qv0Kk_e2FyH1Aa0rWkmr3wapwoycLSc9sJBuZ2BcZLqg0zy  ';
+ 
+
+export const fetchTracks = async () : Promise<SavedTrack[]> => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
     method: 'GET',
     headers: {
@@ -10,7 +13,7 @@ export const fetchTracks = async () => {
   if (!response.ok) {
      throw new Error(`Fetching tracks failed with status ${response.status}`)
    }
-  const data = await response.json() as { items: unknown[] };
+  const data = await response.json() as { items: SavedTrack[] };
 
   return data.items;
 };

4b8405696e37629351801a6570d608f1932de1c2
stiago123 <smarinoj@unal.edu.co>
2024-02-20 20:16:21 +0100

    idk2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { fetchTracks } from './lib/fetchTracks';
+import { useQuery } from '@tanstack/react-query';
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -10,14 +12,22 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
-const [trackIndex, setTrackIndex] = useState(0)
 
-const goToNextTrack = () => {
-  setTrackIndex(trackIndex + 1);
-}
+
+
 
 
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0)
+
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  }
+  const { data: tracks } = useQuery({
+		queryKey: ['tracks'],
+		queryFn: fetchTracks
+});
+  console.log(tracks)
   return (
     <div className="App">
       <header className="App-header">

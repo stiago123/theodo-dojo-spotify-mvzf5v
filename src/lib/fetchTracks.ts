0930bd84ff8b9ba414d732e335965f12d80160ff
stiago123 <smarinoj@unal.edu.co>
2024-02-20 20:24:29 +0100

    idkkk

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,16 @@
-const apiToken: string = '';
+const apiToken = 'BQDfAZg8yOKsEU7lL8rNnDIL2fSoyHhUf2KrpAFGumre6Q1_0Oj-w_ExnGcIcRJGcawIWHM2YUzFudHHB8ch7FxM8qGOl1Z6d6dA0Zq0qUgp9Ccga19C0fi2mTTYwqp_II5Z4NzcDqHLjhnTZY3p9I1W4Qv0Kk_e2FyH1Aa0rWkmr3wapwoycLSc9sJBuZ2BcZLqg0zy  ';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+     throw new Error(`Fetching tracks failed with status ${response.status}`)
+   }
+  const data = await response.json() as { items: unknown[] };
+
+  return data.items;
 };
